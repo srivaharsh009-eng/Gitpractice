d5fd85b527391efea489b37b4006483768f8819f
Harssh <srivaharsh009@gmail.com>
2026-08-01 11:38:35 +0530

    Increased limit to 100

--- practice.py
+++ practice.py
@@ -2,7 +2,7 @@ print("Hello World")
 
 x=int(input("Taking Input: "))
 
-if x<=10:
+if x<=100:
 	print("Hello World " * x)
 else:
-	print("Enter value of x less than or equal to 10")
+	print("Enter value of x less than or equal to 100")
